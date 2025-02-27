a1992f3243fe8bbedf3aa4eb94f047a56b1671fd
elysiany1san <wzf20050707@hotmail.com>
2025-02-27 13:22:50 +0800

    Lab 1 ex 5

--- src/FourToEight.java
+++ src/FourToEight.java
@@ -4,7 +4,6 @@ public class FourToEight {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        // todo: if-else
         if (num >= 4 && num <= 8) {
             System.out.println("YES");
         }
