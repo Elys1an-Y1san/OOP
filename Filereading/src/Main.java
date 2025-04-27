8cda24e38f28654675b6575cc52c8c3e1cfd98f2
Elys1an-Y1san <wzf20050707@hotmail.com>
2025-04-27 21:36:42 +0800

    gogogo出发喽

--- Filereading/src/Main.java
+++ Filereading/src/Main.java
@@ -1,17 +1,28 @@
+import java.io.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import static java.lang.System.out;
+
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        try (FileInputStream stream = new FileInputStream("text.txt")){
-            byte[] bytes = new byte[stream.available()];
-            stream.read(bytes);
-
-
-
+    public static void main(String[] args) {
+        File file = new File ("text.txt");
+        try (FileInputStream in = new FileInputStream(file);
+            FileOutputStream out = new FileOutputStream("xxx.txt")){
+            byte[] bytes = new byte[1024];
+            int len;
+            long total = file.length(), sum = 0;
+            while ((len= in.read(bytes)) != -1) {
+                out.write(bytes, 0 , len);
+                sum += len;
+                System.out.println(sum);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
+
     }
 }
